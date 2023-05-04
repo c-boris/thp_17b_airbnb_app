7bfae34409c1636e61b61a0afb632fe75f39df01
cboris <b.crochet@gmail.com>
2023-05-04 23:48:18 +0200

    add files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Créer 10 villes aléatoires
+10.times do
+  City.create(city_name: Faker::Address.city)
+end
+
+# Créer 10 Dogsitters aléatoires, en assignant une ville aléatoire à chacun
+10.times do
+  dogsitter = Dogsitter.create(name: Faker::Name.name, email: Faker::Internet.email, city_id: rand(1..10))
+end
+
+# Créer 10 Dogs aléatoires, en assignant une ville aléatoire à chacun
+10.times do
+  dog = Dog.create(name: Faker::Creature::Dog.name, breed: Faker::Creature::Dog.breed, city_id: rand(1..10))
+end
